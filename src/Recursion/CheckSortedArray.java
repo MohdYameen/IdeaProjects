f3bdb92fa2fcbc06c12bc404629154241b293cc7
Mohd Yameen <37767352+MohdYameen@users.noreply.github.com>
2024-02-12 18:59:02 +0530

    Recursion added and improved code

--- src/Recursion/CheckSortedArray.java
+++ src/Recursion/CheckSortedArray.java
@@ -2,17 +2,12 @@ package Recursion;
 
 public class CheckSortedArray {
     public static boolean isSorted(int [] arr, int i){
-        if(i == arr.length-1){
-            return true;
-        }
-        if(arr[i] > arr[i+1]){
-            return false;
-        }
+        if(i == arr.length-1) return true;
+        if(arr[i] > arr[i+1]) return false;
         return isSorted(arr, i+1);
     }
     public static void main(String[] args) {
-        int [] arr = {1,2,3,56,5,6};
+        int [] arr = {1,2,3,3,5,6};
         System.out.println(isSorted(arr,0));
-
     }
 }
