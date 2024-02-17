4c8b6577118767c8c691e29b28ac114c6934cc3b
Mohd Yameen <37767352+MohdYameen@users.noreply.github.com>
2024-02-17 12:31:23 +0530

    Update NQueens.java

--- src/Backtrack/NQueens.java
+++ src/Backtrack/NQueens.java
@@ -49,8 +49,8 @@ public class NQueens {
     }
 
     public static void main(String[] args) {
-        int n = 5;
-        char board[][] = new char[n][n];
+        int n = 4;
+        char [][]board = new char[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 board[i][j]='-';
