d0cc793ee213331e0b6eacc4bef248e27818a868
Mohd Yameen <37767352+MohdYameen@users.noreply.github.com>
2024-06-15 00:52:45 +0530

    lcs bu

--- src/DP/LongestCommonSubsequence.java
+++ src/DP/LongestCommonSubsequence.java
@@ -10,7 +10,7 @@ public class LongestCommonSubsequence {
         else return Math.max(lcs(s1,s2,n,m-1), lcs(s1,s2,n-1,m));
     }
 
-    static int lcsMemoization(String s1, String s2, int n, int m, int dp[][]){
+    static int lcsMemoization(String s1, String s2, int n, int m, int[][] dp){
         if(n==0||m==0) return 0;
 
         if(dp[n][m] != -1) return dp[n][m];
@@ -23,6 +23,23 @@ public class LongestCommonSubsequence {
         }
     }
 
+    static int lcsTabulation(String s1, String s2){
+        int [][]dp = new int[s1.length()+1][s2.length()+1];
+
+        for(int i=1; i< dp.length; i++){
+            for(int j=1; j<dp[i].length; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
+
+    }
+
     public static void main(String[] args) {
         String s1 = "abcdge";
         String s2 = "abedg";
@@ -32,6 +49,8 @@ public class LongestCommonSubsequence {
             Arrays.fill(dp[i], -1);
         }
         System.out.println(lcsMemoization(s1,s2,s1.length(), s2.length(), dp));
+
+        System.out.println(lcsTabulation(s1,s2));
 
 
 //        for(int []n:dp){
